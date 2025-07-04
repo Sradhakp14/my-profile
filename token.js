6a46cc4e31c6581f2fa552f8c9a41963b902313f
Sradhakp14 <sradhakp14@gmail.com>
2025-07-04 13:52:27 +0530

    Add files via upload

---
+++ token.js
@@ -0,0 +1,39 @@
+
+
+let userName = "Aarav" ;
+let age = 25 ;
+let isStudent = true ;
+
+
+let msg = "welcome to the world" ;
+let mark = 50 ;
+let pass = false;
+
+let let = "name";
+
+let a = 10;
+let b = 5;
+let sum = a + b ;
+console.log("sum",sum);
+
+let i = 10;
+let m = 5;
+let diff = a - b ;
+console.log("difference",diff);
+
+let x = 15;
+let y = 5;
+let mul = x*y;
+console.log("res",mul);
+
+let d = 25 ;
+let e = 5 ;
+let div = d / e ;
+console.log("answer is",div); 
+
+let totalPrice = 1000;
+let disc = 20;
+let discountAmt = (totalPrice * disc) /100 ;
+let originalPrice = totalPrice - discountAmt ;
+console.log(`your payable amount is ${originalPrice}`);
+
